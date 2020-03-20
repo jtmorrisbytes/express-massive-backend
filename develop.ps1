365620ed3d740255e30dffec185c119a610c5b72
= <=>
2020-03-20 16:14:16 -0500

    prepare to integrate massive

--- develop.ps1
+++ develop.ps1
@@ -1,10 +1,24 @@
 & .\scripts\install.ps1
-& .\scripts\setEnv.ps1
-$cwd = (Get-Location).Path;
-Start-Process -WorkingDirectory ($cwd) powershell {
 
-    Set-Location "./client; npm run start";
+Set-Variable -Name ENV -Value development
+Set-Variable -Name NODE_ENV -Value development
+Set-Variable -Name DEBUG -Value true
+$cwd = (Get-Location).Path;
+Start-Process -WorkingDirectory ("$cwd\client") powershell {
+
+    Set-Location "npm run start";
 }
-Start-Process -WorkingDirectory ($cwd) powershell {
-    Set-Location "./server; npm run dev"
+Start-Process -WorkingDirectory ("$cwd\client") powershell {
+
+    npm run testDev;
 }
+Start-Process -WorkingDirectory ("$cwd\server") powershell {
+    npm run dev
+}
+Start-Process -WorkingDirectory ("$cwd\client") powershell {
+
+    npm run testDev;
+}
+# Start-Process -WorkingDirectory ($cwd) -NoNewWindow powershell {
+#     npx autogitter
+# }
--- scripts/setEnv.ps1
+++
@@ -1,3 +0,0 @@
-Set-Variable -Name ENV -Value development
-Set-Variable -Name NODE_ENV -Value development
-Set-Variable -Name DEBUG -Value true
